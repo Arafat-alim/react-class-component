91a64be8a3342b715d27a9292b24f9be4b6bc6e9
arafat-alim <arafat97alim@gmail.com>
2022-07-23 23:27:43 +0530

    state is added and converted the function component into class based component

--- src/components/GoOut.jsx
+++ src/components/GoOut.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
-export default function GoOut(props) {
-  return (
-    <div>
-      <h1>Should I go out tonight?</h1>
+// export default function GoOut(props) {
+//   return (
+//     <div>
+//       <h1>Should I go out tonight?</h1>
+//       <div>
+//         <h1 onClick={props.handleClick}>{props.isGoing ? "Yes" : "No"}</h1>
+//       </div>
+//     </div>
+//   );
+// }
+
+export default class GoOut extends React.Component {
+  render() {
+    return (
       <div>
-        <h1 onClick={props.handleClick}>{props.isGoing ? "Yes" : "No"}</h1>
+        <h1>Should I go out tonight?</h1>
+        <div>
+          <h1 onClick={this.props.handleClick}>
+            {this.props.isGoing ? "Yes" : "No"}
+          </h1>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/components/GoOutParent.jsx
+++ src/components/GoOutParent.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import GoOut from "./GoOut";
 
-export default function GoOutParent() {
-  // ! state
-  const [isGoing, setIsGoing] = React.useState(true);
-  // ! handle click
-  function handleClick() {
-    setIsGoing((prevState) => !prevState);
+// export default function GoOutParent() {
+//   // ! state
+//   const [isGoing, setIsGoing] = React.useState(true);
+//   // ! handle click
+//   function handleClick() {
+//     setIsGoing((prevState) => !prevState);
+//   }
+//   return (
+//     <div>
+//       <GoOut isGoing={isGoing} handleClick={handleClick} />
+//     </div>
+//   );
+// }
+
+export default class GoOutParent extends React.Component {
+  //! state in class based component
+  state = {
+    isGoing: true,
+  };
+  //! function
+  // function handleClick(){
+  //     this.setState((prevState) => !prevState);
+  // }
+  render() {
+    return (
+      <div>
+        <GoOut isGoing={this.isGoing} handleClick={this.handleClick} />
+      </div>
+    );
   }
-  return (
-    <div>
-      <GoOut isGoing={isGoing} handleClick={handleClick} />
-    </div>
-  );
 }
