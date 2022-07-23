bef7dca6b458af0067fb5e8188ec39326825310c
arafat-alim <arafat97alim@gmail.com>
2022-07-23 23:10:24 +0530

    converted both the components into class based components

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,20 +1,43 @@
 import React from "react";
 
-export default function Greetings() {
-  const date = new Date();
-  const hours = date.getHours();
-  let data;
-  console.log(hours);
-  if (hours < 12) {
-    data = "Morning";
-  } else if (hours >= 12 && hours < 17) {
-    data = "Afternoon";
-  } else {
-    data = "Night";
+// export default function Greetings() {
+// const date = new Date();
+// const hours = date.getHours();
+// let data;
+// console.log(hours);
+// if (hours < 12) {
+//   data = "Morning";
+// } else if (hours >= 12 && hours < 17) {
+//   data = "Afternoon";
+// } else {
+//   data = "Night";
+// }
+//   return (
+//     <div>
+//       <h1>Good {data} to you, Sir or Madam</h1>
+//     </div>
+//   );
+// }
+
+//! Class based component
+
+export default class Greetings extends React.Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let data;
+    console.log(hours);
+    if (hours < 12) {
+      data = "Morning";
+    } else if (hours >= 12 && hours < 17) {
+      data = "Afternoon";
+    } else {
+      data = "Night";
+    }
+    return (
+      <div>
+        <h1>Good {data} to you! Sir or Madam</h1>
+      </div>
+    );
   }
-  return (
-    <div>
-      <h1>Good {data} to you, Sir or Madam</h1>
-    </div>
-  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
-export default function Header(props) {
-  return (
-    <div>
-      <p>Welcome, {props.name}</p>
-    </div>
-  );
+// export default function Header(props) {
+//   return (
+//     <div>
+//       <p>Welcome, {props.name}</p>
+//     </div>
+//   );
+// }
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome, {this.props.name}</p>
+      </div>
+    );
+  }
 }
