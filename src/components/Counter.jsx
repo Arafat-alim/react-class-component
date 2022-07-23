b164ee46f88729b59431e08c042a179ca2de2d7f
arafat-alim <arafat97alim@gmail.com>
2022-07-24 00:09:40 +0530

    added constructor function

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -25,13 +25,20 @@ import React from "react";
 
 export default class Counter extends React.Component {
   //   const [count, setCount] = React.useState(0);
-  state = {
-    count: 0,
-  };
-  add = () => {
+  //! Creating a constructor
+  constructor() {
+    super();
+    this.state = { count: 0 };
+
+    //bind - when we are dealing with normal function
+    this.add = this.add.bind(this);
+  }
+  //if we want to access this type of function for dealing with setState method, then we have to use constructor
+  // for binding and removing the error
+  add() {
     // setCount((prevCount) => prevCount + 1);
     this.setState((prevState) => ({ count: prevState.count + 1 }));
-  };
+  }
   subtract = () => {
     // setCount((prevCount) => prevCount - 1);
     this.setState((prevState) => {
