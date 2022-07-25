11f0f9ecf1c6de8c8b59e5f3fcf07612ebf83f25
arafat-alim <arafat97alim@gmail.com>
2022-07-25 23:19:24 +0530

    created form successfully.

--- src/components/LifeCycle/Form.jsx
+++ src/components/LifeCycle/Form.jsx
@@ -6,20 +6,36 @@ export default class Form extends React.Component {
     lastName: "",
     email: "",
     comments: "",
-    isChecked: "",
+    isFriendly: true,
     employement: "",
     favColor: "",
+  };
+
+  handleChange = (event) => {
+    // console.log(e.target.value);
+    const { name, type, checked, value } = event.target;
+    type === "checkbox"
+      ? this.setState({
+          [name]: checked,
+        })
+      : this.setState({
+          [name]: value,
+        });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
   };
   render() {
     return (
       <div>
         <h1>Form Component</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             placeholder="First Name"
             name="firstName"
             value={this.state.firstName}
+            onChange={this.handleChange}
           />
           <br />
           <input
@@ -27,6 +43,7 @@ export default class Form extends React.Component {
             placeholder="Last Name"
             name="lastName"
             value={this.state.lastName}
+            onChange={this.handleChange}
           />
           <br />
           <input
@@ -34,49 +51,63 @@ export default class Form extends React.Component {
             placeholder="Email"
             name="email"
             value={this.state.email}
+            onChange={this.handleChange}
           />
           <br />
           <textarea
             placeholder="Comment"
             name="comments"
             value={this.state.comments}
+            onChange={this.handleChange}
           />
           <br />
           <input
             type="checkbox"
             id="isFriendly"
-            name="isChecked"
-            value={this.state.isChecked}
+            name="isFriendly"
+            checked={this.state.isFriendly}
+            onChange={this.handleChange}
           />
           <label htmlFor="isFriendly">Are you Friendly?</label>
           <fieldset>
             <legend>Current Employement Status</legend>
             <input
               type="radio"
-              name="employment"
+              name="employement"
               id="unemployed"
+              value="unemployed"
               checked={this.state.employement === "unemployed"}
+              onChange={this.handleChange}
             />
             <label htmlFor="unemployed">Unemployment</label>
             <input
               type="radio"
-              name="employment"
+              name="employement"
               id="part-time"
+              value="part-time"
               checked={this.state.employement === "part-time"}
+              onChange={this.handleChange}
             />
             <label htmlFor="employed">Part Time</label>
             <input
               type="radio"
-              name="employment"
+              name="employement"
+              value="full-time"
               id="full-time"
               checked={this.state.employement === "full-time"}
+              onChange={this.handleChange}
             />
             <label htmlFor="full-time">Full Time</label>
           </fieldset>
           <br />
           <label htmlFor="favColor">What is your Favourite Color?</label>
           <br />
-          <select name="favColor" id="favColor" value={this.state.favColor}>
+          <select
+            name="favColor"
+            id="favColor"
+            value={this.state.favColor}
+            onChange={this.handleChange}
+          >
             <option value="#">----Choose Any Option----</option>
             <option value="red">Red</option>
             <option value="orange">Orange</option>
